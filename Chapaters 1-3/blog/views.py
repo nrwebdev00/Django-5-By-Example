d534d347a6aab7222277c3933e4d42b7697a10ef
Nathon Reed <nrwebdev00@gmail.com>
2025-02-09 16:25:57 -0800

    Chapater 1 Completed

--- Chapaters 1-3/blog/views.py
+++ Chapaters 1-3/blog/views.py
@@ -1,3 +1,13 @@
-from django.shortcuts import render
+from django.shortcuts import render, get_object_or_404
+from .models import Post
 
-# Create your views here.
+def post_list(request):
+    posts = Post.published.all()
+
+    context = {"posts": posts}
+    return render(request, 'blog/list.html', context)
+
+def post_detail(request, id):
+    post = get_object_or_404(Post, id=id, status=Post.Status.PUBLISHED)
+    context = {"post": post}
+    return render( request, 'blog/detail.html', context)
